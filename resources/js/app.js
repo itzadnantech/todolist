05a75465398d7cffbd01be5eba7bfcab1d76a7b0
Adnan Hussain <itzadnantech@gmail.com>
2022-08-15 12:04:52 +0500

    updated

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import app from './vue/app.vue';
+
+createApp(app).mount('#app')
---
+++ resources/js/vue/app.vue
@@ -0,0 +1,3 @@
+<template>
+<h1>Laravel 9, Vite and vuejs 3 is here!</h1>
+</template>
